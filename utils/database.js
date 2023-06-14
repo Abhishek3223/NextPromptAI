28342f9c8760b41e2e2999956873353cd202249f
abhishek ranjan <imabhishekranjann1001@gmail.com>
2023-06-14 15:52:12 +0530

    removed error

--- utils/database.js
+++ utils/database.js
@@ -11,7 +11,7 @@ export const connectToDB = async () => {
   }
 
   try {
-    console.log(process.env.MONGODB_URI);
+
     await mongoose.connect(process.env.MONGODB_URI, {
       dbName: "share_prompt",
       useNewUrlParser: true,
